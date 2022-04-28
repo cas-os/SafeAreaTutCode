d7414b7910995d764980064515293c9a7ce0a185
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 21:13:16 +0200

    Initial Commit

---
+++ SafeAreaTutCode/SafeAreaTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreaTutCodeApp.swift
+//  SafeAreaTutCode
+//
+//  Created by HAL-9001 on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreaTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
